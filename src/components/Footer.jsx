ae826a17a445f5eeb7b50167c5cdd8ed236d0d00
Vanshikaagar21 <vanshika.agar@gmail.com>
2023-07-11 19:17:40 +0530

    add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p>Ⓒ Programmig with Vanshika</p>
+    </div>
+  );
+}
+
+export default Footer;
